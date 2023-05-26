8191861802dc1ea0de962b68bdafe9fdf02b0adc
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2023-05-26 08:38:17 -0400

    added hello print statement

--- src/testdemos23friam/TestDemos23FriAM.java
+++ src/testdemos23friam/TestDemos23FriAM.java
@@ -15,6 +15,7 @@ public class TestDemos23FriAM {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello");
     }
     
 }
