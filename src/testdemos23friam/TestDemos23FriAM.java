f60c9311014e52c10601dc5321ef50b96f13bd07
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2023-05-26 08:36:16 -0400

    first commit

---
+++ src/testdemos23friam/TestDemos23FriAM.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testdemos23friam;
+
+/**
+ *
+ * @author Ronak
+ */
+public class TestDemos23FriAM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
